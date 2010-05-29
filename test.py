b4671054081323f7ffdba4be3b7f569227c1a182
Markus Hametner <fin+github@xbhd.org>
2010-05-29 17:49:52 +0200

    initial version of packet sniffer

---
+++ test.py
@@ -0,0 +1,47 @@
+import pcapy
+from scapy.layers.all import IP, Ether,ICMP, TCP
+import pydb
+import netifaces
+
+interface = 'wlan0'
+interface_address = netifaces.ifaddresses(interface)[2][0]['addr']
+print interface_address
+
+p = pcapy.open_live(interface, 1024, False, 10240)
+
+summary_in = {}
+summary_out = {}
+
+try:
+    (header, payload) = p.next()
+    while header:
+        e = Ether(payload)
+        t = e.getlayer(TCP)
+        if t:
+            i = e.getlayer(IP)
+            print i.dst
+            print interface_address
+            if i.dst==interface_address:
+                print 'incoming'
+                print t.dport
+                print t.sport
+                summary_in.setdefault(t.dport,0)
+                summary_in[t.dport]+=1
+            else:
+                summary_out.setdefault(t.dport,0)
+                summary_out[t.dport]+=1
+
+        if e.haslayer(ICMP):
+            print 'ping'
+
+        second = header.getts()[0] # [1] = miliseconds
+        (header, payload) = p.next()
+except KeyboardInterrupt:
+    pass
+except Exception,e:
+    print e
+    
+
+print summary_in
+print summary_out
+
